8b628569a58c055ada61e51667fa18d384d0b215
Rlcolli4 <col14022@byui.edu>
2015-06-22 14:38:55 -0600

    This has the added CSS to the application page
    Signed-off-by: Rlcolli4 <col14022@byui.edu>

--- index.php
+++ index.php
@@ -1,8 +1,84 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<script src='getSuggestions.js'></script>
-	</head>
-<body>
+    <head>
+        <title>Bucket List App</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src='getSuggestions.js'></script>
+
+<style>
+body {position:relative;
+width:375px;
+height:700px;
+border:solid 1px;
+}
+
+div#header {position:relative;
+			width:375px;
+			height:10%;
+			border:solid ;
+			background-color:#A3AE7E;
+			color:#5C604D;
+			padding: 10px;
+}
+
+div#suggestions {position:relative;
+				width:375px;
+				height:45%;
+				border:solid 1px;
+				background-color:#5C604D;
+				color:#C8CFB4;
+}
+
+div#feed {position:relative;
+				width:375px;
+				height:25%;
+				border:solid 1px;
+				background-color:#5C604D;
+				color:#C8CFB4;
+}
+
+div#footer {position:relative;
+			width:375px;
+			height:20%;
+			border:solid 1px;
+			background-color:#C8CFB4;
+			color:#5C604D;
+}
+
+div#myBucket {position: fixed;
+				left:50px;
+				bottom: 75px;
+}
+
+div#createEvent{position: fixed;
+				left:325px;
+				bottom: 75px;
+}
+        </style>
+</head>
+    
+ <body>                  
+        <!--This Div will contain the Logo and will one of these colors
+       #5C604D	#FFBAD2	#F20056	#E5E7E1	#C8CFB4	#A3AE7E--> 
+        <div id = "header">
+            <img src = "images/Signing_the_contract_64.png" alt = "bucket list">
+        </div>
+        
+        <div id = "feed">
+        <!--This div will hold the swipe button that leads the the activity feed -->
+            <strong>Activity</strong>
+        </div>
+        
+<div id = "suggestions">
+            <!--This div will hold the swipe button that leads the the suggestions feed-->
+            <strong>Suggestions</strong>
+</div>
+
+<div id = "footer">
+            <div id = "myBucket"><a href="w3schools.com"><img src = "images/Bucket_32.png" alt = "Bucket Icon"></a></div>
+             <div id ="createEvent"><a href="w3schools.com"><img src = "images/Add_anchor_point_32.png" alt = "Add to List Icon"></a></div>
+</div>
+
 </body>
 </html>
